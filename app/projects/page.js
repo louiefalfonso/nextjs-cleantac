5abaab195bd7d402720c6f9d405d559cfbf693eb
Louie Alfonso <lfriasalfonso@gmail.com>
2023-10-13 22:00:36 +0100

    nextjs cleantac project
    nextjs cleantac project

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,34 @@
+import BrandArea from '@/components/BrandArea'
 import React from 'react'
+import Link from 'next/link'
+import ProjectsArea from '@/components/ProjectsArea'
 
 export default function page() {
   return (
-    <div>Project page</div>
+    <>
+    <section className="breadcrumb-area breadcrumb-bg" data-background="img/bg/breadcrumb_bg.jpg">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="breadcrumb-content">
+              <h2 className="title">Projects</h2>
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item">
+                    <Link href="/" passHref>Home</Link>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    Projects
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <ProjectsArea/>
+    <BrandArea/>
+    </>
   )
 }
---
+++ components/ProjectsArea.js
@@ -0,0 +1,196 @@
+import React from 'react'
+
+export default function ProjectsArea() {
+  return (
+    <>
+    <section className="inner-project-area-two pt-130 pb-130">
+        <div className="container">
+            <div className="project-item-wrap">
+            <div className="row justify-content-center">
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img01-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Outdoor Cleaning</a>
+                    </h2>
+                    <span>Outdoor cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img02-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Car Cleaning</a>
+                    </h2>
+                    <span>Car Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img12-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Plumbing Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img06-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Kitchen Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img07-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Toilet Cleaning</a>
+                    </h2>
+                    <span>Home cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img05-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Laundry Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img07-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Toilet Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img03-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Home Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img09-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Door Cleaning</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img10-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Spray Cleaning Floor</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img11-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Hotel Room</a>
+                    </h2>
+                    <span>Cleaning</span>
+                    </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-10">
+                <div className="project-item inner-project-item">
+                    <div className="project-thumb">
+                    <a href="project-details.html">
+                        <img src="img/project/project_img08-450x360.webp" alt="" />
+                    </a>
+                    </div>
+                    <div className="project-content">
+                    <h2 className="title">
+                        <a href="project-details.html">Office Cleaning</a>
+                    </h2>
+                    <span>Office Cleaning</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
